12edf6fb1176e054d51c7cfc529ca326647e323b
marek <marek.spisiak165@gmail.com>
2023-10-19 13:07:35 +0200

    started creating header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./containers/HomePage";
+import BlogPage from "./containers/BlogPage";
+
+// ... ďalšie importy ...
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        {/* ... ďalšie trasy ... */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
