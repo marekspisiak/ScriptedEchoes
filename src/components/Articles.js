4b58930da69ad868f675545318b20648f79f3429
marek <marek.spisiak165@gmail.com>
2023-10-24 08:54:41 +0200

    pridanie tlacidla pri articles

---
+++ src/components/Articles.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
+
+function Articles({ articles }) {
+  return (
+    <Row>
+      {articles.map((article, index) => (
+        <Col md={4} key={index}>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={article.image} />
+            <Card.Body>
+              <Card.Title>{article.title}</Card.Title>
+              <Card.Text>{article.description}</Card.Text>
+              <Card.Footer>
+                <small className="text-muted">{article.date}</small>
+              </Card.Footer>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default Articles;
--- src/components/HomeArticles.js
+++ src/components/HomeArticles.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { Card, Row, Col } from "react-bootstrap";
+import Articles from "./Articles";
+import { Button } from "react-bootstrap";
 
 function HomeArticles({ title, articles }) {
   return (
     <div className="articles-section">
-      <h2>{title}</h2>
-      <Row>
-        {articles.map((article, index) => (
-          <Col md={4} key={index}>
-            <Card className="mb-4">
-              <Card.Img variant="top" src={article.image} />
-              <Card.Body>
-                <Card.Title>{article.title}</Card.Title>
-                <Card.Text>{article.description}</Card.Text>
-                <Card.Footer>
-                  <small className="text-muted">{article.date}</small>
-                </Card.Footer>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <div className="d-flex justify-content-start align-items-center mb-3">
+        <h2 className="mr-2" style={{ marginRight: "20px" }}>
+          {title}
+        </h2>{" "}
+        {/* Menší margin-right pre h2 */}
+        <Button
+          variant="outline-primary"
+          size="sm"
+          href="/path_to_all_articles"
+        >
+          Zobraziť všetko
+        </Button>
+      </div>
+
+      <Articles articles={articles} />
     </div>
   );
 }
