ad679998741f8b4f4270b0e30c574c344c6ff838
marek <marek.spisiak165@gmail.com>
2023-10-25 12:03:48 +0200

    uprava headera

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,15 +3,7 @@ import StartWritingButton from "./buttons/StartWritingButton";
 
 function HeroSection() {
   return (
-    <Container
-      className="hero-section d-flex flex-column justify-content-center align-items-center"
-      style={{
-        height: "70vh",
-        backgroundImage: 'url("cesta_k_obrazku.jpg")',
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <Container className="hero-section d-flex flex-column justify-content-center align-items-center">
       <h1 className="display-3 text-center mb-3">Zdieľajte svoj príbeh</h1>
       <p className="lead text-center mb-4">
         Zdieľajte svoje skúsenosti, nápady a myšlienky so svetom na
