b939ef8d78355f6b3620158cf5f81364dba6e9b7
marek <marek.spisiak165@gmail.com>
2023-10-25 23:01:32 +0200

    pridany scroll to top

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import LoginPage from "./containers/LoginPage";
 import RegisterPage from "./containers/RegisterPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
-// ... ďalšie importy ...
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} />
