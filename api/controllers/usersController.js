d32ad6743274c3278fcfb496fa2b449e21125d86
marek <marek.spisiak165@gmail.com>
2023-12-07 05:32:10 +0100

    Refactor usersController.js to use User model instead of Post model

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,161 +1,92 @@
-const Post = require("../models/post.js");
+const User = require("../models/user.js");
 
-exports.getAllPosts = async (req, res) => {
+exports.getAllUsers = async (req, res) => {
   try {
-    const posts = await Post.findAll();
-    res.json(posts);
+    const users = await User.findAll();
+    res.json(users);
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-exports.getPostById = async (req, res) => {
+exports.getUserById = async (req, res) => {
   try {
-    const post = await Post.findByPk(req.params.id);
-    if (post) {
-      res.json(post);
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      res.json(user);
     } else {
-      res.status(404).send("Príspevok nebol nájdený");
+      res.status(404).send("Používateľ nebol nájdený");
     }
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-exports.createPost = async (req, res) => {
-  const { title, content, author_id } = req.body;
-
-  if (!title || !content || !author_id) {
-    return res.status(400).send("Názov, obsah a autor sú povinné údaje.");
-  }
-
+exports.getUserProfileById = async (req, res) => {
   try {
-    const newPost = await Post.create({ title, content, author_id });
-    res.status(201).json(newPost);
+    const auth0Id = req.params.id;
+    const user = await User.findOne({
+      where: { auth0_id: auth0Id },
+      attributes: ["username", "email", "user_id"],
+    });
+
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send("Užívateľ nebol nájdený");
+    }
   } catch (error) {
-    console.error("Chyba pri vytváraní príspevku:", error);
-    res.status(500).send(error.message);
+    console.error("Chyba pri získavaní užívateľa: ", error);
+    res.status(500).send("Interná chyba servera");
   }
 };
 
-exports.deletePost = async (req, res) => {
+exports.createUser = async (req, res) => {
   try {
-    const auth0Id = req.auth.payload.sub;
-    const postId = req.params.id;
+    const existingUser = await User.findOne({
+      where: { auth0_id: req.body.auth0_id },
+    });
 
-    const user = await User.findOne({ where: { auth0_id: auth0Id } });
-
-    if (!user) {
-      return res.status(404).send("Užívateľ nebol nájdený");
-    }
-
-    const post = await Post.findOne({ where: { post_id: postId } });
-
-    if (!post) {
-      return res.status(404).send("Príspevok nebol nájdený");
-    }
-
-    if (post.author_id !== user.user_id) {
+    if (existingUser) {
       return res
-        .status(403)
-        .send("Nemáte oprávnenie odstrániť tento príspevok");
+        .status(409)
+        .json({ message: "Užívateľ s daným Auth0 ID už existuje." });
     }
-    const User = require("../models/user.js");
 
-    exports.getAllUsers = async (req, res) => {
-      try {
-        const users = await User.findAll();
-        res.json(users);
-      } catch (error) {
-        res.status(500).send(error.message);
-      }
-    };
-
-    exports.getUserById = async (req, res) => {
-      try {
-        const user = await User.findByPk(req.params.id);
-        if (user) {
-          res.json(user);
-        } else {
-          res.status(404).send("Používateľ nebol nájdený");
-        }
-      } catch (error) {
-        res.status(500).send(error.message);
-      }
-    };
-
-    exports.getUserProfileById = async (req, res) => {
-      try {
-        const auth0Id = req.params.id;
-        const user = await User.findOne({
-          where: { auth0_id: auth0Id },
-          attributes: ["username", "email", "user_id"],
-        });
-
-        if (user) {
-          res.json(user);
-        } else {
-          res.status(404).send("Užívateľ nebol nájdený");
-        }
-      } catch (error) {
-        console.error("Chyba pri získavaní užívateľa: ", error);
-        res.status(500).send("Interná chyba servera");
-      }
-    };
-
-    exports.createUser = async (req, res) => {
-      try {
-        const existingUser = await User.findOne({
-          where: { auth0_id: req.body.auth0_id },
-        });
-
-        if (existingUser) {
-          return res
-            .status(409)
-            .json({ message: "Užívateľ s daným Auth0 ID už existuje." });
-        }
-
-        const newUser = await User.create(req.body);
-        res.status(201).json(newUser);
-      } catch (error) {
-        console.log(error);
-        res.status(500).send(error.message);
-      }
-    };
-
-    exports.updateUser = async (req, res) => {
-      try {
-        const { username } = req.body;
-
-        const validUsernameRegex = /^[a-zA-Z0-9_-]+$/;
-
-        if (!username) {
-          return res.status(400).send("Username je povinný");
-        }
-
-        if (!validUsernameRegex.test(username)) {
-          return res.status(400).send("Neplatný formát užívateľského mena");
-        }
-
-        const result = await User.update(
-          { username },
-          { where: { auth0_id: req.params.id } }
-        );
-
-        if (result[0] > 0) {
-          res.send("Užívateľ bol úspešne aktualizovaný.");
-        } else {
-          res.status(404).send("Užívateľ nebol nájdený alebo žiadna zmena.");
-        }
-      } catch (error) {
-        console.error("Chyba pri aktualizácii užívateľa:", error);
-        res.status(500).send("Interná chyba servera");
-      }
-    };
-
-    await Post.destroy({ where: { post_id: postId } });
-    res.status(204).send();
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
   } catch (error) {
+    console.log(error);
     res.status(500).send(error.message);
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    const validUsernameRegex = /^[a-zA-Z0-9_-]+$/;
+
+    if (!username) {
+      return res.status(400).send("Username je povinný");
+    }
+
+    if (!validUsernameRegex.test(username)) {
+      return res.status(400).send("Neplatný formát užívateľského mena");
+    }
+
+    const result = await User.update(
+      { username },
+      { where: { auth0_id: req.params.id } }
+    );
+
+    if (result[0] > 0) {
+      res.send("Užívateľ bol úspešne aktualizovaný.");
+    } else {
+      res.status(404).send("Užívateľ nebol nájdený alebo žiadna zmena.");
+    }
+  } catch (error) {
+    console.error("Chyba pri aktualizácii užívateľa:", error);
+    res.status(500).send("Interná chyba servera");
   }
 };
