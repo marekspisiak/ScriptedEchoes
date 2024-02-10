6dc90a13277469e9bb77e24374be9f95bc6a0efa
marek <marek.spisiak165@gmail.com>
2024-02-10 23:32:20 +0100

    upraveny edit post a pridany qhat you see is what you get editor

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,7 +1,11 @@
+// PostForm.jsx
 import React, { useEffect, useState } from "react";
-import { Form, Container, Row, Col, Alert } from "react-bootstrap";
-import Button from "./buttons/Button";
+import { Form } from "react-bootstrap";
 import axios from "axios";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import styles from "./PostForm.module.scss"; // Uistite sa, že ste vytvorili tento SCSS modul
+import Button from "./buttons/Button";
 
 const PostForm = ({
   handleSubmitParent,
@@ -9,14 +13,14 @@ const PostForm = ({
   initialContent = "",
   initialDescription = "",
   initialCategory = "",
-  initialImage = "", // Pridaná nová inicializačná hodnota pre obrázok, ak je to potrebné
+  initialImage = "",
 }) => {
   const [title, setTitle] = useState(initialTitle);
   const [content, setContent] = useState(initialContent);
   const [description, setDescription] = useState(initialDescription);
   const [categories, setCategories] = useState([]);
   const [category, setCategory] = useState(initialCategory);
-  const [image, setImage] = useState(initialImage); // Stav pre uchovanie nahraného obrázka
+  const [image, setImage] = useState(initialImage);
 
   const lengthLimits = {
     title: 20,
@@ -25,16 +29,8 @@ const PostForm = ({
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    // Pripravíme dáta pre odoslanie, vrátane nahraného obrázka
     const data = { title, content, description, category, image };
-
-    await handleSubmitParent(data); // Zmena na odosielanie FormData objektu
-    // setTitle("");
-    // setContent("");
-    // setDescription("");
-    // setCategory("");
-    // setImage(""); // Reset stavu obrázka
+    await handleSubmitParent(data);
   };
 
   const handleFetchCategories = async () => {
@@ -51,74 +47,72 @@ const PostForm = ({
   }, []);
 
   return (
-    <Form onSubmit={handleSubmit} encType="multipart/form-data">
-      <Form.Group controlId="blogTitle">
-        <Form.Label>Názov</Form.Label>
+    <Form onSubmit={handleSubmit} className={styles.formContainer}>
+      <Form.Group controlId="blogTitle" className={styles.formGroup}>
+        <Form.Label className={styles.label}>Názov</Form.Label>
         <Form.Control
           type="text"
           placeholder="Zadajte názov blogu"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           maxLength={lengthLimits.title}
+          className={styles.input}
         />
-        <Form.Text>
+        <Form.Text className={styles.formText}>
           Zostáva {lengthLimits.title - title.length} znakov
         </Form.Text>
       </Form.Group>
 
-      <Form.Group controlId="blogDescription">
-        <Form.Label>Popisok</Form.Label>
+      <Form.Group controlId="blogDescription" className={styles.formGroup}>
+        <Form.Label className={styles.label}>Popisok</Form.Label>
         <Form.Control
           type="text"
           placeholder="Zadajte popis blogu"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           maxLength={lengthLimits.description}
+          className={styles.input}
         />
-        <Form.Text>
+        <Form.Text className={styles.formText}>
           Zostáva {lengthLimits.description - description.length} znakov
         </Form.Text>
       </Form.Group>
 
-      <Form.Group controlId="blogCategory">
-        <Form.Label>Kategória</Form.Label>
+      <Form.Group controlId="blogCategory" className={styles.formGroup}>
+        <Form.Label className={styles.label}>Kategória</Form.Label>
         <Form.Select
           value={category || ""}
           onChange={(e) => setCategory(e.target.value)}
+          className={styles.select}
         >
           <option value="">Vyberte kategóriu</option>
-          {categories &&
-            categories.map((category) => (
-              <option
-                key={category.category_id}
-                value={category.category_id || ""}
-              >
-                {category.name}
-              </option>
-            ))}
+          {categories.map((category) => (
+            <option key={category.category_id} value={category.category_id}>
+              {category.name}
+            </option>
+          ))}
         </Form.Select>
       </Form.Group>
 
-      <Form.Group controlId="blogContent">
-        <Form.Label>Obsah</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={5}
-          placeholder="Zadajte obsah blogu"
+      <Form.Group controlId="blogContent" className={styles.formGroup}>
+        <Form.Label className={styles.label}>Obsah</Form.Label>
+        <ReactQuill
           value={content}
-          onChange={(e) => setContent(e.target.value)}
+          onChange={setContent}
+          className={styles.quill}
         />
       </Form.Group>
 
-      <Form.Group controlId="blogImage">
-        <Form.Label>Náhľadový obrázok</Form.Label>
+      <Form.Group controlId="blogImage" className={styles.formGroup}>
+        <Form.Label className={styles.label}>Náhľadový obrázok</Form.Label>
         <Form.Control
           type="file"
           onChange={(e) => setImage(e.target.files[0])}
+          className={styles.fileInput}
         />
       </Form.Group>
 
-      <Button className={"mt-2"} type="submit" variant={"primary"}>
+      <Button type="submit" className={styles.button} variant={"primary"}>
         Potvrdit
       </Button>
     </Form>
